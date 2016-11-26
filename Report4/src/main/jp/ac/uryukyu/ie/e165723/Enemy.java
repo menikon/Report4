200298d822efd5d79267ea0954eb0890e44f3573
Takahiro Sakamoto <e165723@ie.u-ryukyu.ac.jp>
2016-11-26 14:12:51 +0900

    レポート４

--- Report4/src/main/jp/ac/uryukyu/ie/e165723/Enemy.java
+++ Report4/src/main/jp/ac/uryukyu/ie/e165723/Enemy.java
@@ -40,12 +40,13 @@ public class Enemy {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param hero 攻撃対象
      */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+    public void attack(Hero hero) {
+        if (!dead) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }
     }
-
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
@@ -53,7 +54,7 @@ public class Enemy {
      */
     public void wounded(int damage){
         hitPoint -= damage;
-        if( hitPoint < 0 ) {
+        if( hitPoint <= 0 ) {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
